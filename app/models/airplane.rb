de8040e5dd8f529dc6c2a9ed75d839a1fcd6de32
Francisco Suero <suerofrancisco@yahoo.com>
2017-04-26 19:45:15 -0400

    finished enqueue, need to do priority

--- app/models/airplane.rb
+++ app/models/airplane.rb
@@ -4,8 +4,17 @@ class Airplane < ApplicationRecord
   validates :cargo_type, presence: true
 
   def self.enqueue(params)
-    p params
+    plane = Airplane.new(params)
+    plane.priority = 0 
+    return plane.errors unless plane.valid?
+    plane.priority = calculate_priority(params)
+    plane.save
+    plane
   end
 
+  def self.calculate_priority(params)
+    #the lower the number the sooner the plane is dequeued. 
+    4
+  end
 end
  
--- spec/models/airplane_spec.rb
+++ spec/models/airplane_spec.rb
@@ -27,5 +27,38 @@ RSpec.describe Airplane, type: :model do
       expect(plane.valid?).to be true
     end
   end
+
+  context "#enqueue" do
+    let(:valid_params) {
+      {
+        name: "Flight 283",
+        cargo_type: "passenger",
+        size: "large"
+      }
+    }
+    context "invalid params" do
+      it "Returns errors if given invalid parameters" do 
+        expect(Airplane.enqueue({})).to be_a(ActiveModel::Errors)
+      end
+    end
+    context "valid params" do
+      it "returns a new airplane record" do 
+        expect(Airplane.enqueue(valid_params)).to be_a(Airplane)
+      end
+      it "the record is persisted to the db" do
+        expect{Airplane.enqueue(valid_params)}.to change{Airplane.count}.by(1)
+      end
+      it "the record has a priority" do 
+        expect(Airplane.enqueue(valid_params).priority).to be_a(Integer)
+      end
+    end
+
+    context "#calculate priority" do 
+      it "priority 1 if"
+      it "priority 2 if"
+      it "priority 3 if"
+      it "priority 4 if"
+    end 
+  end
 end
  
