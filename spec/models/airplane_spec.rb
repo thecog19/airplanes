703d7a69ba99570f5ea395e856beac34a56b66e2
Francisco Suero <suerofrancisco@yahoo.com>
2017-04-27 01:38:09 -0400

    fixed spacing

--- spec/models/airplane_spec.rb
+++ spec/models/airplane_spec.rb
@@ -61,53 +61,52 @@ RSpec.describe Airplane, type: :model do
       
       it "priority for small passenger" do
        params = {"size" => "small", "cargo_type" => "passenger"}
-        expect(Airplane.calculate_priority(params)).to eq(2)
+       expect(Airplane.calculate_priority(params)).to eq(2)
       end
 
       it "priority for big cargo" do 
         params = {"size" => "big", "cargo_type" => "cargo"}
         expect(Airplane.calculate_priority(params)).to eq(3)
       end
-      
+
       it "priority for small cargo" do 
         params = {"size" => "small", "cargo_type" => "cargo"}
         expect(Airplane.calculate_priority(params)).to eq(4)
       end
     end 
-
-    context "#dequeue" do
-      context "there are no planes" do
-        it "returns false" do
-          expect(Airplane.dequeue).to be false
-        end
-      end
-
-      context "there are planes" do
-        it "deletes a record" do
-          plane.save
-          expect{Airplane.dequeue}.to change{Airplane.count}.by(-1)
-        end
-
-        it "returns the deleted record" do
-          plane.save
-          expect(Airplane.dequeue).to eq(plane)
-        end
-
-        it "deletes higher priority planes first" do
-          plane.save
-          plane2 = Airplane.create(name: "blue", priority: 4, cargo_type: "cargo", size: "small")
-          plane3 = Airplane.create(name: "red", priority: 1, cargo_type: "passenger", size: "large")
-          expect(Airplane.dequeue).to eq(plane3)
-        end
-
-        it "deletes later date if priority is tied" do
-          plane.save
-          plane2 = Airplane.create(name: "red", priority: 3, cargo_type: "cargo", size: "large")
-          plane3 = Airplane.create(name: "blue", priority: 4, cargo_type: "cargo", size: "small")
-          expect(Airplane.dequeue).to eq(plane)
-        end
-      end
-    end 
   end
+
+  context "#dequeue" do
+    context "there are no planes" do
+      it "returns false" do
+        expect(Airplane.dequeue).to be false
+      end
+    end
+
+    context "there are planes" do
+      it "deletes a record" do
+        plane.save
+        expect{Airplane.dequeue}.to change{Airplane.count}.by(-1)
+      end
+
+      it "returns the deleted record" do
+        plane.save
+        expect(Airplane.dequeue).to eq(plane)
+      end
+
+      it "deletes higher priority planes first" do
+        plane.save
+        plane2 = Airplane.create(name: "blue", priority: 4, cargo_type: "cargo", size: "small")
+        plane3 = Airplane.create(name: "red", priority: 1, cargo_type: "passenger", size: "large")
+        expect(Airplane.dequeue).to eq(plane3)
+      end
+
+      it "deletes later date if priority is tied" do
+        plane.save
+        plane2 = Airplane.create(name: "red", priority: 3, cargo_type: "cargo", size: "large")
+        plane3 = Airplane.create(name: "blue", priority: 4, cargo_type: "cargo", size: "small")
+        expect(Airplane.dequeue).to eq(plane)
+      end
+    end
+  end 
 end
- 
